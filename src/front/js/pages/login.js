396f0dbd295a2565e731a6b5cd7750eab4cbd4d9
Keialex17 <115053607+Keialex17@users.noreply.github.com>
2023-06-01 19:59:19 +0000

    add login

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Login =() =>{
+    return(
+        <div>
+            <p>
+            Hola soy el Login                
+            </p>
+        </div>
+    )
+}
