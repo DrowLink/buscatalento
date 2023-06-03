d889b8baf254fbd3e555a361d5b376a3b598c75b
Keialex17 <115053607+Keialex17@users.noreply.github.com>
2023-06-03 18:03:10 +0000

    carrusel listo

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,9 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
+import carrusel1 from "../../img/carrusel1.jpg";
+import carrusel2 from "../../img/carrusel2.jpg";
+import carrusel3 from "../../img/carrusel3.jpg";
 import "../../styles/home.css";
 import { Link } from "react-router-dom";
 
@@ -11,36 +14,36 @@ export const Home = () => {
 
 	<div>
 		<div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src="https://cdn.pixabay.com/photo/2017/10/17/16/10/fantasy-2861107_1280.jpg" className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-		<button type="button" className="btn btn-outline-info d-flex justify-content-start">Ver mas</button>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src="https://cdn.pixabay.com/photo/2017/10/17/16/10/fantasy-2861107_1280.jpg" className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-		<button type="button" className="btn btn-outline-info d-flex justify-content-start">Ver mas</button>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src="https://cdn.pixabay.com/photo/2017/10/17/16/10/fantasy-2861107_1280.jpg" className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-		<button type="button" className="btn btn-outline-info d-flex justify-content-start">Ver mas</button>
-      </div>
-    </div>
+  			<div className="carousel-indicators">
+    			<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    			<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    			<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  			</div>
+ 		 <div className="carousel-inner">
+   			 <div className="carousel-item active">
+      			<img src={carrusel1} id="carrusel1" className="d-block w-100" alt="..."/>
+     				 <div className="carousel-caption d-none d-md-block">
+        				<h5>First slide label</h5>
+        					<p>Some representative placeholder content for the first slide.</p>
+								<button type="button" className="btn btn-outline-info d-flex justify-content-start">Ver mas</button>
+     				 </div>
+    		</div>
+    		<div className="carousel-item">
+     		 	<img src={carrusel2} id="carrusel2" className="d-block w-100" alt="..."/>
+     			 	<div className="carousel-caption d-none d-md-block">
+        				<h5>Second slide label</h5>
+        					<p>Some representative placeholder content for the second slide.</p>
+								<button type="button" className="btn btn-outline-info d-flex justify-content-start">Ver mas</button>
+     				 </div>
+   			 </div>
+   			 <div className="carousel-item">
+      			<img src={carrusel3} id="carrusel3" className="d-block w-100" alt="..."/>
+    				<div className="carousel-caption d-none d-md-block">
+        				<h5>Third slide label</h5>
+       						 <p>Some representative placeholder content for the third slide.</p>
+								<button type="button" className="btn btn-outline-info d-flex justify-content-start">Ver mas</button>
+      				</div>
+    		</div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
