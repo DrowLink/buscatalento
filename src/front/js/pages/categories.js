706876279ddbff5f11f19c212a5516fc7e9b9734
Keialex17 <115053607+Keialex17@users.noreply.github.com>
2023-06-03 21:29:54 +0000

    listo categorias

--- src/front/js/pages/categories.js
+++ src/front/js/pages/categories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/categories.css"
 import { Link } from "react-router-dom";
 
 export const Categories = () => {
@@ -6,15 +7,42 @@ export const Categories = () => {
         <div>
             <div className="container-fluid bg-secondary bg-opacity-25 m-0">
     		    <div className="container py-4">
-                    <div className="row d-flex justify-content-around">
-                        <div className="col-md-4">
-                            <img src="..." className="rounded-circle" alt="..."/>
+                    <div className="row">
+                        <div className="col-md-12 d-flex justify-content-around">
+                            <h1>Selecciona las categorias por las que te gustaria empezar</h1>
                         </div>
-                        <div className="col-md-4">
-                             <img src="..." className="rounded-circle" alt="..."/>
+                    </div>
+                    <div className="row d-flex justify-content-around mt-5">
+                        <div className="col-md-4 d-flex justify-content-center">
+                            <img src="https://wellaggio.com/wp-content/uploads/2015/09/la-importancia-de-las-imagenes-en-el-dise%C3%B1o-web1.jpg" id="categories" className="rounded-circle" alt="..."/>
                         </div>
-                        <div className="col-md-4">
-                            <img src="..." className="rounded-circle" alt="..."/>
+                        <div className="col-md-4 d-flex justify-content-center">
+                             <img src="https://wellaggio.com/wp-content/uploads/2015/09/la-importancia-de-las-imagenes-en-el-dise%C3%B1o-web1.jpg" id="categories" className="rounded-circle" alt="..."/>
+                        </div>
+                        <div className="col-md-4 d-flex justify-content-center">
+                            <img src="https://wellaggio.com/wp-content/uploads/2015/09/la-importancia-de-las-imagenes-en-el-dise%C3%B1o-web1.jpg" id="categories" className="rounded-circle" alt="..."/>
+                        </div>  
+                    </div>
+                    <div className="row d-flex justify-content-around mt-5">
+                        <div className="col-md-4 d-flex justify-content-center">
+                            <img src="https://wellaggio.com/wp-content/uploads/2015/09/la-importancia-de-las-imagenes-en-el-dise%C3%B1o-web1.jpg" id="categories" className="rounded-circle" alt="..."/>
+                        </div>
+                        <div className="col-md-4 d-flex justify-content-center">
+                            <img src="https://wellaggio.com/wp-content/uploads/2015/09/la-importancia-de-las-imagenes-en-el-dise%C3%B1o-web1.jpg" id="categories" className="rounded-circle" alt="..."/>
+                        </div>
+                        <div className="col-md-4 d-flex justify-content-center">
+                            <img src="https://wellaggio.com/wp-content/uploads/2015/09/la-importancia-de-las-imagenes-en-el-dise%C3%B1o-web1.jpg" id="categories" className="rounded-circle" alt="..."/>
+                        </div>
+                    </div>
+                    <div className="row d-flex justify-content-around mt-5">
+                        <div className="col-md-4 d-flex justify-content-center">
+                            <img src="https://wellaggio.com/wp-content/uploads/2015/09/la-importancia-de-las-imagenes-en-el-dise%C3%B1o-web1.jpg" id="categories" className="rounded-circle" alt="..."/>
+                        </div>
+                        <div className="col-md-4 d-flex justify-content-center">
+                            <img src="https://wellaggio.com/wp-content/uploads/2015/09/la-importancia-de-las-imagenes-en-el-dise%C3%B1o-web1.jpg" id="categories" className="rounded-circle" alt="..."/>
+                        </div>
+                        <div className="col-md-4 d-flex justify-content-center">
+                            <img src="https://wellaggio.com/wp-content/uploads/2015/09/la-importancia-de-las-imagenes-en-el-dise%C3%B1o-web1.jpg" id="categories" className="rounded-circle" alt="..."/>
                         </div>
                     </div>
 			    </div>
